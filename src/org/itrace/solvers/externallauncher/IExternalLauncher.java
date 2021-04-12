bb69df9182401e996d344076c391509b1df70a61
Kang-il Park <kangil.park@huskers.unl.edu>
2021-04-12 09:32:57 -0500

    A commit

---
+++ src/org/itrace/solvers/externallauncher/IExternalLauncher.java
@@ -0,0 +1,9 @@
+package org.itrace.solvers.externallauncher;
+
+import org.itrace.solvers.ISolver;
+import java.util.List;
+
+public interface IExternalLauncher extends ISolver {
+    void start();
+}
+
